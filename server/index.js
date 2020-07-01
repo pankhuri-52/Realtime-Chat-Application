10974aebc0865ca3b9388f49d2e905db73276591
Pankhuri <pankhuritrikha@gmail.com>
2020-07-01 14:07:19 +0530

    Inculcated our helper functions in the backend into the file index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const socketio = require('socket.io'); //real-time
 const http = require('http'); //http is slow as compared to socket.io
 
+//import all the helper functions of users from users.js
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./users');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -15,12 +18,33 @@ const io = socketio(server);
 //Socket.io code
 // this socket is going to be connect as a  client side socket
 io.on('connection',(socket) => {
-    console.log('We have a new connection');
     
     // we got this data from chat.js in the chat folder from the frontend part
     socket.on('join',({ name, room }, callback) => {
-        console.log(name,room);
+          const {error, user} = addUser({ id: socket.id, name, room});
+
+          if(error) return callback(error);
+
+          //Simple welcome message for any user
+          socket.emit('message', {user : 'admin', text : `${user.name} , welcome to the room ${user.room}`});
+
+          //socket.broadcast will send message to everyone beside that particular user
+          socket.broadcast.to(user.name).emit('message', {user : 'admin', text: `${user.name} has joined!!`});
+          
+          // joins the particular user to the room
+          socket.join(user.room);
+
+          callback();
     });
+
+    // User generated messages will be sendMessage and admin will be message, waiting for the frontend to send message
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message',{user : user.name, text: message});
+
+        callback();
+    })
 
     socket.on('disconnect',() => {
         console.log('User had left');
